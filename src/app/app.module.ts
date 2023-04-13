78009cb29b58d317e841b0e88fa8ab314687cbfc
DonaMariya <donamariyajohn123@gmail.com>
2023-04-13 13:26:12 +0530

    job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { JobpostingComponent } from './jobposting/jobposting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    JobpostingComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,17 @@
+// import { Component } from '@angular/core';
+
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent {
+
+// }
+
 import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
 
 
 @Component({
